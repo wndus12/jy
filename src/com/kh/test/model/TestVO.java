ec19d6790dc488849d375c1c49e752a70126b79b
user1 <user1@user1-PC>
2019-07-26 10:43:36 +0900

    TestVO 미완성 상태

---
+++ src/com/kh/test/model/TestVO.java
@@ -0,0 +1,15 @@
+package com.kh.test.model;
+
+public class TestVO {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
